0aed9f4c26f400e448db76d3008284bdba15ae43
79969 <m.byckoff2015@yandex.ru>
2023-05-02 00:30:12 +0300

    feat: add missing endpoint - venue/get_all

--- src/main/java/com/cityconcert/controller/VenueController.java
+++ src/main/java/com/cityconcert/controller/VenueController.java
@@ -4,15 +4,22 @@ import com.cityconcert.domain.dto.VenueDTO;
 import com.cityconcert.service.impl.VenueServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "venue")
+@RequestMapping(value = "/venue")
 public class VenueController {
-    private final VenueServiceImpl vsi;
+    private final VenueServiceImpl venueService;
     public VenueController(VenueServiceImpl vsi) {
-        this.vsi = vsi;
+        this.venueService = vsi;
     }
     @GetMapping(value = "/get/{id}", produces = {"application/json", "application/xml"})
     public VenueDTO getVenue(@PathVariable Long id) {
-        return vsi.findOne(id).get();
+        return venueService.findOne(id).get();
+    }
+
+    @GetMapping(value = "/get_all", produces = {"application/json", "application/xml"})
+    public List<VenueDTO> getVenues() {
+        return venueService.findAll();
     }
 }
