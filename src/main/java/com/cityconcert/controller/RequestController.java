54a828664005f2438c20717a12e4d8014c95f180
79969 <m.byckoff2015@yandex.ru>
2023-05-05 18:11:13 +0300

    feat: add mvc components for Request entity

---
+++ src/main/java/com/cityconcert/controller/RequestController.java
@@ -0,0 +1,48 @@
+package com.cityconcert.controller;
+
+import com.cityconcert.domain.dto.RequestDTO;
+import com.cityconcert.service.impl.RequestServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Tag(name="Запросы", description = "Методы, взаимодействующие с запросами на обмен билетов и поиск компании на мероприятие")
+@RequestMapping(value = "request")
+public class RequestController {
+    private final RequestServiceImpl requestService;
+
+    public RequestController(RequestServiceImpl requestService) {
+        this.requestService = requestService;
+    }
+
+    @GetMapping(value = "/get/{id}", produces = {"application/json", "application/xml"})
+    @Operation(summary = "Получаем экземпляр запроса по его Id")
+    public RequestDTO getEvent(@PathVariable Long id) {
+        return requestService.findOne(id).orElse(null);
+    }
+
+
+    @GetMapping(value = "/get_all", produces = {"application/json", "application/xml"})
+    @Operation(summary = "Отображаем все запросы")
+    public List<RequestDTO> displayAllEvents() {
+        return requestService.findAll();
+    }
+
+    @PostMapping(value = "/update", produces = {"application/json", "application/xml"})
+    @Operation(summary = "Обновляем экземпляр пользователя")
+    public RequestDTO updateRequest(@RequestBody RequestDTO request) {
+        return requestService.update(request);
+    }
+
+
+    @DeleteMapping(value = "/delete/{id}",
+            produces = {"application/json", "application/xml"})
+    @Operation(summary = "Удаляем экземпляр пользователя по его Id")
+    public void deleteRequest(@PathVariable Long id) {
+        requestService.delete(id);
+    }
+
+}
---
+++ src/main/java/com/cityconcert/service/impl/RequestServiceImpl.java
@@ -0,0 +1,71 @@
+package com.cityconcert.service.impl;
+
+import com.cityconcert.domain.Request;
+import com.cityconcert.domain.dto.RequestDTO;
+import com.cityconcert.mapper.RequestMapper;
+import com.cityconcert.repository.RequestRepository;
+import com.cityconcert.service.RequestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Service Implementation for managing {@link Request}.
+ */
+@Service
+@Transactional
+public class RequestServiceImpl implements RequestService {
+
+    private final Logger log = LoggerFactory.getLogger(EventServiceImpl.class);
+
+    private final RequestRepository requestRepository;
+
+    private final RequestMapper requestMapper;
+
+    public RequestServiceImpl(RequestRepository requestRepository, RequestMapper requestMapper) {
+        this.requestRepository = requestRepository;
+        this.requestMapper = requestMapper;
+    }
+
+
+    @Override
+    public RequestDTO save(RequestDTO requestDTO) {
+        log.debug("Request to save Request : {}", requestDTO);
+        Request request = requestMapper.toEntity(requestDTO);
+        request = requestRepository.save(request);
+        return requestMapper.toDto(request);
+    }
+
+    @Override
+    public RequestDTO update(RequestDTO requestDTO) {
+        log.debug("Request to update Request : {}", requestDTO);
+        Request request = requestMapper.toEntity(requestDTO);
+        request = requestRepository.save(request);
+        return requestMapper.toDto(request);
+    }
+
+    @Override
+    public Optional<RequestDTO> findOne(Long id) {
+        log.debug("Request to get Request : {}", id);
+        return requestRepository.findById(id).map(requestMapper::toDto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Request : {}", id);
+        requestRepository.deleteById(id);
+    }
+
+    @Override
+    public List<RequestDTO> findAll() {
+        log.debug("Request to get all Requests");
+        return requestRepository.findAll().stream().map(requestMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+
+    }
+}
